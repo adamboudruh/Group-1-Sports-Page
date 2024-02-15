7a032626f7135bc2dead9c329eee6dd8fe35d5a0
cammichbursc@gmail.com <cammichbursc@gmail.com>
2024-02-14 23:36:50 -0600

    Added the Comments to Game relationship and added game to the model export

--- models/index.js
+++ models/index.js
@@ -1,15 +1,25 @@
 const User = require('./User');
 const Comments = require('./Comments')
+const Game = require('./Game')
 
 Comments.belongsTo(User, { 
     foreignKey: 'user_id' 
 });
 
 User.hasMany(Comments,{
-    foreignKey: "comments_id"
+    foreignKey: "user_id"
 });
+
+Comments.belongsTo(Game, { 
+    foreignKey: 'game_id' 
+});
+
+
+
+
 
 module.exports = { 
     User,
     Comments,
+    Game
 };
