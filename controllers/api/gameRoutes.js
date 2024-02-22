02bfd3f7a505aa94f0614bac4b29be058a99d609
Adam Boudraa <adamboudraa@gmail.com>
2024-02-21 18:00:33 -0800

    Tweaks to game routes, displaying upcoming games works now

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { Game } = require('../../models');
 
-router.get('/upcoming', async (req, res) => {
+router.get('/load', async (req, res) => {
     try {
       //Add some code that automatically takes todays day and adds a day to it
 
@@ -15,7 +15,6 @@ router.get('/upcoming', async (req, res) => {
       if (!response.ok) throw new Error('Error in retrieving data.');
       const data = await response.json();
       // const data = "THIS IS A TEST";
-      console.info(data);
       const currentDateTime = new Date();
 
       const filePath = path.resolve(__dirname, '../../seeds/gameData.json');
@@ -24,19 +23,36 @@ router.get('/upcoming', async (req, res) => {
       fs.writeFile(textPath, currentDateTime.toLocaleString(), (err) =>err ? console.log("Error in writing file: "+err) : console.log('Success!')); 
 
       res.json(data); // Send the retrieved data as JSON response to client
-      
     }
     catch (err) { console.info("Error in retrieving data from API: "+err) };
   });
 
-  router.get('/singlegame/:game_id', async (req, res) => {
+router.get('/upcoming', async (req, res) => {
+  try{
+    const upcomingGameData = await Game.findAll();
+    
+    const upcomingGames = upcomingGameData.map(game => game.get({plain: true}));
+    console.log(upcomingGames);
+
+    res.render('landingpage', {
+      upcomingGames,
+    })
+  }
+  catch (err) {
+    console.error(err);
+  }
+})
+
+
+// Route to get a game object containing all of a game's info and render it
+router.get('/singlegame/:game_id', async (req, res) => {
     try{
       const gameData = await Game.findByPk(req.params.game_id);
       
       const game = gameData.get({ plain: true });
       console.log(game);
 
-      res.render('game', {
+      res.render('singlegame', {
         game,
       })
     }
